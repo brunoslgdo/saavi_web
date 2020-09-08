2e64a45fc6ca365f865d6ee064c80a09a1cfe81b
brunoslgdo <brunoslgdo@gmail.com>
2020-09-07 22:25:46 -0500

    se finalizan los cambios pedidos por el cliente

--- dist/js/show-tooltip.js
+++ dist/js/show-tooltip.js
@@ -1,14 +1,29 @@
-// const showTooltip = (iconHamburguerId, toolId) => {
-// 	let iconHamburguer = document.getElementById(iconHamburguerId),
-// 		nav = document.getElementById(navId);
-// 	if(iconHamburguer && nav) {
-// 		iconHamburguer.addEventListener('click', () => {
-// 			iconHamburguer.classList.toggle('close');
-// 			nav.classList.toggle('show');
-// 		});
-// 	}
-// }
+// const tooltipShow = (arrowId, tooltipId, tooltipCloseId) => {
+// 	let arrow = document.getElementById(arrowId)
+//         tooltip = document.getElementById(tooltipId)
+//         tooltipClose = document.getElementById(tooltipCloseId)
 
-// showMenu('js-icon-hamburguer', 'js-nav');
-// showMenu('js-icon-hamburguer-en', 'js-nav-en');
+//     if(arrow && tooltip) {
+//         arrow.addEventListener('click', () => {
+//             arrow.classList.toggle('arrow-down')
+//             tooltip.classList.toggle('tooltip-show')
+//         })
+//     }
+    
+    // if(tooltipClose && tooltip && ) {
+    //     tooltipClose.addEventListener('click', () => {
+    //         arrow.classList.toggle('arrow-down')
+    //         tooltip.classList.remove('tooltip-show')
+    //     })
+    // }
+    
+// }    
+
+// tooltipShow('supplier-one', 'tooltip-one', 'tooltip-close-one')
+// tooltipShow('supplier-two', 'tooltip-two', 'tooltip-close-two')
+// tooltipShow('supplier-three', 'tooltip-three', 'tooltip-close-three')
+// tooltipShow('supplier-four', 'tooltip-four', 'tooltip-close-four')
+// tooltipShow('supplier-five', 'tooltip-five', 'tooltip-close-five')
+// tooltipShow('supplier-six', 'tooltip-six', 'tooltip-close-six')
+
 
